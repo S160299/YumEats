b3ee1e0d9739e760aea1aa74da4b7c0445a659a5
S160299 <nareshbobbili0004@gmail.com>
2023-11-18 15:50:12 +0530

    updated

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -7,7 +7,7 @@ const TopPicks = () => {
   return (
     <>
       <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1>
-      <div className='hidden lg:flex max-w-screen-xl mx-auto py-2 px-2'>
+      <div className='lg:flex max-w-screen-xl mx-auto py-2 px-2'>
         <Splide options={{perPage: 4,gap:"0.5em",drag:"free", arrows: false}}>
         {topPicks.map((item) => {
           return (
